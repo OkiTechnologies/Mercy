97fcbde4cfeff2cbb536d35675eadec61a557104
Solomon Ochepa <solomon.ochepa@gmail.com>
2021-12-29 03:36:08 +0100

    Update Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,43 +1,47 @@
 <template>
-  <Head title="Welcome" />
-
-  <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
-  >
-    <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-      <Link
-        v-if="$page.props.user"
-        :href="route('dashboard')"
-        class="text-sm text-gray-700 underline"
-      >
-        Dashboard
-      </Link>
-
-      <template v-else>
-        <Link :href="route('login')" class="text-sm text-gray-700 underline">
-          Log in
-        </Link>
-
+  <guest-layout title="Welcome">
+    <div
+      class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+    >
+      <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
         <Link
-          v-if="canRegister"
-          :href="route('register')"
-          class="ml-4 text-sm text-gray-700 underline"
+          v-if="$page.props.user"
+          :href="route('dashboard')"
+          class="text-sm text-gray-700 underline"
         >
-          Register
+          <i class="fas fa-desktop"></i>&nbsp;Dashboard
         </Link>
-      </template>
+
+        <template v-else>
+          <Link :href="route('login')" class="text-sm text-gray-700 underline">
+            Log in
+          </Link>
+
+          <Link
+            v-if="canRegister"
+            :href="route('register')"
+            class="ml-4 text-sm text-gray-700 underline"
+          >
+            Register
+          </Link>
+        </template>
+      </div>
+
+      <section>...welcome!</section>
     </div>
-  </div>
+  </guest-layout>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 export default defineComponent({
   components: {
     Head,
-    Link
+    Link,
+    GuestLayout
   },
 
   props: {
