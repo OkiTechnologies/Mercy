f54868c65f0aaa998dd9755bfc3ff25a5b0e9fe2
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-03 10:09:53 +0100

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -20210,11 +20210,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   data: function data() {
     return {
-      asset: this.$page.props.app.asset + '/guest/'
+      asset: this.$page.props.app.asset + '/guest/',
+      next_event: Object
     };
+  },
+  mounted: function mounted() {
+    var _this = this;
+
+    axios__WEBPACK_IMPORTED_MODULE_0___default().get(route('api.event.next')).then(function (response) {
+      return _this.next_event = response.data.next_event;
+    });
   }
 });
 
@@ -23853,7 +23864,27 @@ var _hoisted_3 = {
 };
 var _hoisted_4 = ["src"];
 
-var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"gc_index2_event_heading_wrapper\"><h3>Next Upcoming Event</h3></div><div class=\"gc_event_heading_cont_wrapper\"><h4>Sharing Our Faith &amp; Love To Children</h4></div><div class=\"gc_event_heading_cont_time_wrapper\"><p><i class=\"fa fa-calendar\"></i> 14 May 2017</p><p class=\"event_time\"><i class=\"fa fa-clock-o\"></i> @ 8 to 11 AM</p></div>", 3);
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "gc_index2_event_heading_wrapper"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h3", null, "Next Upcoming Event")], -1
+/* HOISTED */
+);
+
+var _hoisted_6 = {
+  "class": "gc_event_heading_cont_wrapper"
+};
+
+var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "gc_event_heading_cont_time_wrapper"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", null, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("i", {
+  "class": "fa fa-calendar"
+}), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" 14 May 2017")]), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+  "class": "event_time"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("i", {
+  "class": "fa fa-clock-o"
+}), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" @ 8 to 11 AM")])], -1
+/* HOISTED */
+);
 
 var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"gc_upcoming_event_timer_main_wrapper\"><div class=\"gc_upcoming_event_timer_wrapper\"><div id=\"clockdiv\"><div><span class=\"days\"></span><div class=\"smalltext\">Days</div></div><div><span class=\"hours\"></span><div class=\"smalltext\">hrs</div></div><div><span class=\"minutes\"></span><div class=\"smalltext\">Min</div></div><div><span class=\"seconds\"></span><div class=\"smalltext\">Sec</div></div></div><div class=\"gc_event_timer_btn\"><ul><li><a href=\"#\">JOIN US!</a></li></ul></div></div></div>", 1);
 
@@ -23863,7 +23894,9 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     alt: "icon"
   }, null, 8
   /* PROPS */
-  , _hoisted_4)]), _hoisted_5])]), _hoisted_8]);
+  , _hoisted_4)]), _hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h4", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.next_event.title), 1
+  /* TEXT */
+  )]), _hoisted_7])]), _hoisted_8]);
 }
 
 /***/ }),
