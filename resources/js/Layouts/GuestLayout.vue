07282c131c19a20110b016ce8e467d8c78c0709f
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-03 06:44:01 +0100

    Update GuestLayout.vue

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -3,6 +3,12 @@
     <Head :title="title" />
 
     <div class="min-h-screen bg-gray-100">
+      <Preloader />
+      <TopScroll />
+      <TopHeader />
+      <Header />
+      <upcoming-event />
+
       <!-- Page Content -->
       <main>
         <slot></slot>
@@ -14,15 +20,34 @@
 <script>
 import { defineComponent } from 'vue';
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import Preloader from '@/Layouts/Guest/Preloader.vue';
+import TopScroll from '@/Layouts/Guest/TopScroll.vue';
+import TopHeader from '@/Layouts/Guest/TopHeader.vue';
+import Header from '@/Layouts/Guest/Header.vue';
+import Slider from '@/Layouts/Guest/Slider.vue';
+import UpcomingEvent from './Guest/UpcomingEvent.vue';
 
 export default defineComponent({
+  components: {
+    Head,
+    Link,
+    Preloader,
+    TopScroll,
+    TopHeader,
+    Header,
+    Slider,
+    UpcomingEvent
+  },
+
   props: {
     title: String
   },
 
-  components: {
-    Head,
-    Link
+  data() {
+    return {
+      // user_id: this.$page.props.user.id,
+      asset: this.$page.props.app.asset + '/guest/'
+    };
   },
 
   methods: {
