40226aaee39461851fa77f740c94883c6d3ab6c6
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-03 06:43:52 +0100

    Update guest.blade.php

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -18,15 +18,15 @@
 	<!-- Styles -->
 	<link rel="stylesheet" href="{{ mix('assets/css/app.css') }}">
 
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/animate.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/font-awesome.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/owl.carousel.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/owl.theme.default.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/magnific-popup.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/fonts.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/reset.css" />
-	<link rel="stylesheet" type="text/css" href={{ asset('guest') }}/css/style2.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/animate.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/font-awesome.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/owl.carousel.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/owl.theme.default.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/magnific-popup.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/fonts.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/style2.css" />
 
 	<!-- favicon links -->
 	<link rel="shortcut icon" type="image/png" href="images/header/favicon.png" />
@@ -36,22 +36,21 @@
 <body class="font-sans antialiased">
 	@inertia
 
-
 	<!-- Scripts -->
 	@routes
 	<script src="{{ mix('assets/js/app.js') }}" defer></script>
 
 	<!-- JS -->
-	<script type="text/javascript" src={{ asset('guest') }}/js/jquery_min.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/bootstrap.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/jquery.menu-aim.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/owl.carousel.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/jquery.shuffle.min.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/modernizr.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/jquery.magnific-popup.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/jquery.countTo.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/jquery.inview.min.js"></script>
-	<script type="text/javascript" src={{ asset('guest') }}/js/custom2.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery_min.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/bootstrap.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery.menu-aim.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/owl.carousel.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery.shuffle.min.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/modernizr.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery.countTo.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery.inview.min.js"></script>
+	<script type="text/javascript" src="{{ asset('guest') }}/js/custom2.js"></script>
 
 	<script>
 		function openNav() {
