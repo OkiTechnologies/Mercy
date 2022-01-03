6a7bae5c95c5b6774c397c2bc0a2f4aeacda77d2
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-03 10:08:58 +0100

    Update 2022_01_03_023352_create_places_table.php

--- database/migrations/2022_01_03_023352_create_places_table.php
+++ database/migrations/2022_01_03_023352_create_places_table.php
@@ -4,28 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlacesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('places', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+class CreatePlacesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('places', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+			$table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('places');
-    }
+			$table->unique(['name'], 'place');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('places');
+	}
 }
