aeda5a6d8e581c171399cffef8f1ac99a3a158cc
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-03 06:42:46 +0100

    Create Preloader.vue

---
+++ resources/js/Layouts/Guest/Preloader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="preloader">
+    <div id="status">
+      <img
+        :src="asset + 'images/header/preloader.gif'"
+        id="preloader_image"
+        alt="loader"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      asset: this.$page.props.app.asset + '/guest/'
+    };
+  }
+};
+</script>
