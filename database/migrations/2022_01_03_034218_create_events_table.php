695b1ed8419efbe97a87195ca6c5d19adb9426b7
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-13 05:22:44 +0100

    Update 2022_01_03_034218_create_events_table.php

--- database/migrations/2022_01_03_034218_create_events_table.php
+++ database/migrations/2022_01_03_034218_create_events_table.php
@@ -16,23 +16,11 @@ class CreateEventsTable extends Migration {
 			$table->string('title', 140);
 			$table->string('slug', 140);
 			$table->text('description')->nullable();
+			$table->timestamp('started_at')->nullable();
+			$table->timestamp('ended_at')->nullable();
 			$table->timestamps();
 
 			$table->unique(['title', 'created_at'], 'event');
-		});
-
-		Schema::create('event_schedules', function (Blueprint $table) {
-			$table->id();
-			$table->foreignId('event_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
-			$table->string('subtitle', 140)->nullable();
-			$table->string('slug', 140)->nullable();
-			$table->text('description')->nullable();
-			$table->timestamp('started_at')->nullable();
-			$table->timestamp('ended_at')->nullable();
-			$table->foreignId('place_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
-			$table->timestamps();
-
-			$table->unique(['event_id', 'subtitle', 'started_at', 'place_id'], 'schedule');
 		});
 	}
 
@@ -42,7 +30,6 @@ class CreateEventsTable extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::dropIfExists('event_schedules');
 		Schema::dropIfExists('events');
 	}
 }
