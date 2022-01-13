550676e35067dfc29adaaae27878b80daa5f14ce
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-13 05:13:33 +0100

    Update guest.blade.php
    Temporary remove mix()

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -16,7 +16,7 @@
 	<meta name="MobileOptimized" content="320" />
 
 	<!-- Styles -->
-	<link rel="stylesheet" href="{{ mix('assets/css/app.css') }}">
+	<link rel="stylesheet" href="{{ asset('') }}/css/app.css">
 
 	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/animate.css" />
 	<link rel="stylesheet" type="text/css" href="{{ asset('guest') }}/css/bootstrap.css" />
@@ -38,7 +38,7 @@
 
 	<!-- Scripts -->
 	@routes
-	<script src="{{ mix('assets/js/app.js') }}" defer></script>
+	<script src="{{ asset('') }}/js/app.js" defer></script>
 
 	<!-- JS -->
 	<script type="text/javascript" src="{{ asset('guest') }}/js/jquery_min.js"></script>
