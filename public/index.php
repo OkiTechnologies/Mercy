b8417c41b8fdb115c38659a5cff87de6a028afaa
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-13 05:22:01 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+define('APP_DIR', __DIR__ . '/../');
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists(APP_DIR . 'storage/framework/maintenance.php')) {
+	require APP_DIR . 'storage/framework/maintenance.php';
 }
 
 /*
@@ -31,7 +32,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require APP_DIR . 'vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,12 +45,12 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once APP_DIR . 'bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
+	$request = Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
