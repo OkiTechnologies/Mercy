f6d36998573dd39d82b74471efd880e331640edc
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-14 02:07:08 +0100

    Update Timeline.php

--- app/Models/Timeline.php
+++ app/Models/Timeline.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Timeline extends Model
-{
-    use HasFactory;
+class Timeline extends Model {
+	use HasFactory;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var string[]
+	 */
+	protected $fillable = [
+		'timeable_type', 'timeable_id', 'venue_id', 'title', 'subtitle', 'slug', 'description',
+		'started_at', 'ended_at', 'repeat', 'interval', 'tags'
+	];
+
+	function timeable() {
+		return $this->morphTo();
+	}
 }
