ab88b5e20034bf5c74be915623ca6c2597301dfd
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-28 12:58:33 +0100

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
+use App\Models\Setting;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,7 +16,12 @@ class Controller extends BaseController {
 	public $app = [];
 
 	public function __construct() {
-		$this->app['name'] = config('app.name');
+		$this->app['name']	= Setting::where('name', 'name')->value('value');
+		$this->app['menus']	= Menu::all(['name', 'slug', 'params'])
+			->map(function ($menu) {
+				$menu->params = json_decode($menu->params);
+				return $menu;
+			});
 
 		Inertia::share('app', $this->app);
 	}
