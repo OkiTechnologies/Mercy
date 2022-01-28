e53ee54ff7f7bed07d9809066f098b823fe6a923
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-28 12:58:23 +0100

    Create Testimonies

---
+++ resources/js/Layouts/Guest/Testimonies.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="gc_client_main_wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="gc_client_slider_wrapper">
+            <div class="owl-carousel owl-theme">
+              <div class="item">
+                <div class="gc_client_slide_main_wrapper">
+                  <div class="gc_client_cont_wrapper">
+                    <p>
+                      “ <span>Grace Community</span> Church provides tools, teams, and
+                      times to help individuals – and the church family as a whole – pray.
+                      Prayer is vitally important to your relationship with
+                      <span>God</span>. We would love to pray for you, a friend, or a
+                      family member. We believe that actions fueled and guided by the
+                      <span>Holy Spirit</span> in accordance to God’s.”
+                    </p>
+                  </div>
+                  <div class="gc_client_img_nd_social_wrapper">
+                    <div class="gc_client_img_wrapper">
+                      <img
+                        src="images/content/client_img.jpg"
+                        class="img-responsive"
+                        alt="client_img"
+                      />
+                    </div>
+                    <div class="gc_client_social_wrapper">
+                      <p>- by <span>Merry Jain</span> <span>(Church Memeber)</span></p>
+                      <ul>
+                        <li>
+                          <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gc_client_slide_main_wrapper">
+                  <div class="gc_client_cont_wrapper">
+                    <p>
+                      “ <span>Grace Community</span> Church provides tools, teams, and
+                      times to help individuals – and the church family as a whole – pray.
+                      Prayer is vitally important to your relationship with
+                      <span>God</span>. We would love to pray for you, a friend, or a
+                      family member. We believe that actions fueled and guided by the
+                      <span>Holy Spirit</span> in accordance to God’s.”
+                    </p>
+                  </div>
+                  <div class="gc_client_img_nd_social_wrapper">
+                    <div class="gc_client_img_wrapper">
+                      <img
+                        src="images/content/client_img.jpg"
+                        class="img-responsive"
+                        alt="client_img"
+                      />
+                    </div>
+                    <div class="gc_client_social_wrapper">
+                      <p>- by <span>Merry Jain</span> <span>(Church Memeber)</span></p>
+                      <ul>
+                        <li>
+                          <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="gc_client_slide_main_wrapper">
+                  <div class="gc_client_cont_wrapper">
+                    <p>
+                      “ <span>Grace Community</span> Church provides tools, teams, and
+                      times to help individuals – and the church family as a whole – pray.
+                      Prayer is vitally important to your relationship with
+                      <span>God</span>. We would love to pray for you, a friend, or a
+                      family member. We believe that actions fueled and guided by the
+                      <span>Holy Spirit</span> in accordance to God’s.”
+                    </p>
+                  </div>
+                  <div class="gc_client_img_nd_social_wrapper">
+                    <div class="gc_client_img_wrapper">
+                      <img
+                        src="images/content/client_img.jpg"
+                        class="img-responsive"
+                        alt="client_img"
+                      />
+                    </div>
+                    <div class="gc_client_social_wrapper">
+                      <p>- by <span>Merry Jain</span> <span>(Church Memeber)</span></p>
+                      <ul>
+                        <li>
+                          <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                        </li>
+                        <li>
+                          <a href="#"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      app: this.$page.props.app,
+      asset: this.$page.props.app.asset + '/guest/'
+      // testimonies: Object
+    };
+  }
+};
+</script>
