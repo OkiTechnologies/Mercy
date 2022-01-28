f17bb0d290ec5862ff25eec69a5187bcf2fec58e
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-28 12:56:26 +0100

    Create SettingController.php

---
+++ app/Http/Controllers/Api/SettingController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Setting;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SettingController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		$settings = Setting::all()->pluck(['value'], 'name');
+		//->load(['category']);
+
+		return response([
+			'settings' => $settings
+		]);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Models\Setting  $setting
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Setting $setting) {
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Models\Setting  $setting
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Setting $setting) {
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Models\Setting  $setting
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Setting $setting) {
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Models\Setting  $setting
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Setting $setting) {
+		//
+	}
+}
