49e301e914672fdec6b83149f4fcf4e8465a8740
Solomon Ochepa <solomon.ochepa@gmail.com>
2022-01-28 12:56:33 +0100

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 
-class Setting extends Model
-{
-    use HasFactory;
+class Setting extends Model {
+	use HasFactory;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var string[]
+	 */
+	protected $fillable = [
+		'name', 'slug', 'category_id', 'value'
+	];
+
+	public function category() {
+		return $this->hasOne(App\Models\Category::class);
+	}
 }
